483ee902f3b1a2b298f838cc191f933999029ce3
GonzaloHirsch <hirschgonzalo@gmail.com>
2023-07-23 07:28:15 +0100

    fix: reduced deployment package for Lambda

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -6,5 +6,5 @@ locals {
   policy_dynamo_name = "policy-alexa-lambda-dynamod"
   policy_logs_name   = "policy-alexa-lambda-execution"
   target_location    = "../.build/${local.payload_name}"
-  package_excludes   = [".github", ".git", ".build", "terraform", ".gitignore", "README.md", "LICENSE"]
+  package_excludes   = [".github", ".git", ".build", ".releaserc.json", ".env", ".eslintrc.json", "docs", ".prettierrc", "terraform", ".gitignore", "README.md", "LICENSE"]
 }
